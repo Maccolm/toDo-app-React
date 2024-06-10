c91c3791219df518f26f8202fee9d1e0a5c41606
Vitalii Shapovalov <martyit@ukr.net>
2024-06-10 18:42:09 +0200

    Prepare for deploying on vercel

---
+++ api/todos.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const dataPath = path.join(__dirname, '..', 'db.json');
+  const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+  if (req.method === 'GET') {
+    res.status(200).json(data.todos);
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+};
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import {v4 as uuidv4} from 'uuid'
 import List from "./List";
 import { useEffect, useState } from "react";
