49ccaa720c4a13fd7ef7ee49e9c9188a877a4ea0
Vitalii Shapovalov <martyit@ukr.net>
2024-06-17 15:25:20 +0200

    installing CORS

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 6001;
+
+// Дозвіл CORS для конкретного домену
+app.use(cors({
+  origin: 'https://to-do-9hypdq2z8-maccolms-projects.vercel.app', // Дозволений домен
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Дозволені методи
+  allowedHeaders: ['Content-Type', 'Authorization'] // Дозволені заголовки
+}));
+
+// Парсинг JSON-тіл запитів
+app.use(express.json());
+
+// Маршрути для вашого API
+app.get('/api/todos', (req, res) => {
+  res.json({ message: 'Отримання списку todo' });
+});
+
+app.post('/api/todos', (req, res) => {
+  res.json({ message: 'Додавання нового todo' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
