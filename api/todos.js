f9b94d981cf1e7c43c24ca4451f44ac1bbe25835
Vitalii Shapovalov <martyit@ukr.net>
2024-06-17 10:19:57 +0200

    setting configuration, adding CORS for using server part of the project

--- api/todos.js
+++ api/todos.js
@@ -2,6 +2,10 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 export default async function handler(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', 'https://to-do-be4b2neow-maccolms-projects.vercel.app');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	
 	const dataPath = path.join(process.cwd(), 'data.json');
 	const data = JSON.parse(await fs.readFile(dataPath, 'utf8'));
 
