c25631282db5f10bb126c1e5cca31c7213c48cb4
Vitalii Shapovalov <martyit@ukr.net>
2024-06-18 10:12:31 +0200

    fetch functions for todos

--- api/todos.js
+++ api/todos.js
@@ -1,57 +1,61 @@
 import { promises as fs } from 'fs';
 import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://to-do-app-gold-iota.vercel.app/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', 'https://to-do-app-gold-iota.vercel.app/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    const dataPath = path.join(process.cwd(), 'data.json');
+  const dataPath = path.join(process.cwd(), 'data.json');
+  const data = JSON.parse(await fs.readFile(dataPath, 'utf8'));
 
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
+  const { userId, id } = req.query;
+
+  if (!userId) {
+    return res.status(400).json({ message: 'userId is required' });
+  }
+
+  if (req.method === 'GET') {
+    const userTodos = data.todos.filter(todo => todo.userId === userId);
+
+    if (id) {
+      const todo = userTodos.find(todo => todo.id === id);
+      if (todo) {
+        res.status(200).json(todo);
+      } else {
+        res.status(404).json({ message: 'Todo not found' });
+      }
+    } else {
+      res.status(200).json(userTodos);
     }
+  } else if (req.method === 'POST') {
+    const newTodo = req.body;
+    data.todos.push(newTodo);
+    await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+    res.status(201).json(newTodo);
+  } else if (req.method === 'PUT') {
+    const updatedTodo = req.body;
 
-    try {
-        const data = JSON.parse(await fs.readFile(dataPath, 'utf8'));
+    const index = data.todos.findIndex(todo => todo.id === id && todo.userId === userId);
 
-        if (req.method === 'GET') {
-            const userId = req.query.userId;
-            const userTodos = data.todos.filter(todo => todo.userId === userId);
-            res.status(200).json(userTodos);
-        } else if (req.method === 'POST') {
-            const newTodo = req.body;
-            newTodo.id = uuidv4();
-            data.todos.push(newTodo);
-            await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
-            res.status(201).json(newTodo);
-        } else if (req.method === 'PUT') {
-            const updatedTodo = req.body;
-            const index = data.todos.findIndex(todo => todo.id === updatedTodo.id);
-            if (index !== -1) {
-                data.todos[index] = updatedTodo;
-                await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
-                res.status(200).json(updatedTodo);
-            } else {
-                res.status(404).json({ message: 'Todo not found' });
-            }
-        } else if (req.method === 'DELETE') {
-            const { id } = req.query;
-            const updatedTodos = data.todos.filter(todo => todo.id !== id);
-            if (updatedTodos.length !== data.todos.length) {
-                data.todos = updatedTodos;
-                await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
-                res.status(200).json({ message: 'Todo deleted' });
-            } else {
-                res.status(404).json({ message: 'Todo not found' });
-            }
-        } else {
-            res.status(405).json({ message: "Method not allowed" });
-        }
-    } catch (error) {
-        console.error('Error handling request', error);
-        res.status(500).json({ message: 'Internal Server Error' });
+    if (index !== -1) {
+      data.todos[index] = { ...data.todos[index], ...updatedTodo };
+      await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+      res.status(200).json(data.todos[index]);
+    } else {
+      res.status(404).json({ message: 'Todo not found' });
     }
+  } else if (req.method === 'DELETE') {
+    const index = data.todos.findIndex(todo => todo.id === id && todo.userId === userId);
+
+    if (index !== -1) {
+      const deletedTodo = data.todos.splice(index, 1);
+      await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+      res.status(200).json(deletedTodo[0]);
+    } else {
+      res.status(404).json({ message: 'Todo not found' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
 }
