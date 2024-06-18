0c260901f1c28dbceca9822cae6ef1126bf73e36
Vitalii Shapovalov <martyit@ukr.net>
2024-06-18 10:26:44 +0200

    try to catch the error of editing

--- api/todos.js
+++ api/todos.js
@@ -2,60 +2,65 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
+try{
   res.setHeader('Access-Control-Allow-Origin', 'https://to-do-app-gold-iota.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   const dataPath = path.join(process.cwd(), 'data.json');
-  const data = JSON.parse(await fs.readFile(dataPath, 'utf8'));
+    const data = JSON.parse(await fs.readFile(dataPath, 'utf8'));
 
-  const { userId, id } = req.query;
+    const { userId, id } = req.query;
 
-  if (!userId) {
-    return res.status(400).json({ message: 'userId is required' });
-  }
+    if (!userId) {
+      return res.status(400).json({ message: 'userId is required' });
+    }
 
-  if (req.method === 'GET') {
-    const userTodos = data.todos.filter(todo => todo.userId === userId);
+    if (req.method === 'GET') {
+      const userTodos = data.todos.filter(todo => todo.userId === userId);
 
-    if (id) {
-      const todo = userTodos.find(todo => todo.id === id);
-      if (todo) {
-        res.status(200).json(todo);
+      if (id) {
+        const todo = userTodos.find(todo => todo.id === id);
+        if (todo) {
+          res.status(200).json(todo);
+        } else {
+          res.status(404).json({ message: 'Todo not found' });
+        }
+      } else {
+        res.status(200).json(userTodos);
+      }
+    } else if (req.method === 'POST') {
+      const newTodo = req.body;
+      data.todos.push(newTodo);
+      await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+      res.status(201).json(newTodo);
+    } else if (req.method === 'PUT') {
+      const updatedTodo = req.body;
+
+      const index = data.todos.findIndex(todo => todo.id === id && todo.userId === userId);
+
+      if (index !== -1) {
+        data.todos[index] = { ...data.todos[index], ...updatedTodo };
+        await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+        res.status(200).json(data.todos[index]);
+      } else {
+        res.status(404).json({ message: 'Todo not found' });
+      }
+    } else if (req.method === 'DELETE') {
+      const index = data.todos.findIndex(todo => todo.id === id && todo.userId === userId);
+
+      if (index !== -1) {
+        const deletedTodo = data.todos.splice(index, 1);
+        await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+        res.status(200).json(deletedTodo[0]);
       } else {
         res.status(404).json({ message: 'Todo not found' });
       }
     } else {
-      res.status(200).json(userTodos);
+      res.status(405).json({ message: 'Method not allowed' });
     }
-  } else if (req.method === 'POST') {
-    const newTodo = req.body;
-    data.todos.push(newTodo);
-    await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
-    res.status(201).json(newTodo);
-  } else if (req.method === 'PUT') {
-    const updatedTodo = req.body;
-
-    const index = data.todos.findIndex(todo => todo.id === id && todo.userId === userId);
-
-    if (index !== -1) {
-      data.todos[index] = { ...data.todos[index], ...updatedTodo };
-      await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
-      res.status(200).json(data.todos[index]);
-    } else {
-      res.status(404).json({ message: 'Todo not found' });
-    }
-  } else if (req.method === 'DELETE') {
-    const index = data.todos.findIndex(todo => todo.id === id && todo.userId === userId);
-
-    if (index !== -1) {
-      const deletedTodo = data.todos.splice(index, 1);
-      await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
-      res.status(200).json(deletedTodo[0]);
-    } else {
-      res.status(404).json({ message: 'Todo not found' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
   }
 }
